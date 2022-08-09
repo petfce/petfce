26fb7c0ce17c0ff0344bff6657a5f28e3f12581e
petfce <lutfialfurjani@yahoo.com>
2022-08-09 13:08:13 +0200

    first commit

---
+++ Examples/Mavenproject1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author almadkhal6
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+               if(i%2==0){
+                   System.out.println(i);
+               }
+            }
+        
+    }
+}
