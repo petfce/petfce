66a9812bae4036160739e5e73e0a31f965963a98
petfce <lutfialfurjani@yahoo.com>
2022-08-11 08:38:24 +0200

    Training

---
+++ Mavenproject7.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.mavenproject7;
+
+/**
+ *
+ * @author almadkhal6
+ */
+public class Mavenproject7 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        
+   // inside an inner loop or simply nested loops
+// Importing generic Classes/Files
+
+		// Outer loop for iteration
+		for (int i = 1; i <= 4; i++) {
+			// Inner loop for iteration
+			for (int j = 1; j <= 3; j++) {
+				if (i == 3 && j == 2) {
+					// Continue statement in inner loop to
+					// skip the execution when i==3 and j==2
+					continue;
+				}
+				// Print elements to showcase keyword affect
+				System.out.println(i + " * " + j);
+			}
+		}
+	}
+}     
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
